30dfe3d5fcaf8814f8bb4d0dedff773c1158caf3
liumenghua <18611144373@163.com>
2021-10-28 17:33:13 +0800

    增加sheetIndex

--- src/main/java/com/angla/plugins/excel/export/processer/impl/EnumFormatProcessor.java
+++ src/main/java/com/angla/plugins/excel/export/processer/impl/EnumFormatProcessor.java
@@ -20,18 +20,25 @@ public class EnumFormatProcessor implements ExportAnnoProcessor {
         if (null == value) {
             return null;
         }
+        Integer result;
+        Class<? extends ISEnum> enumClass = exportFieldBean.getEnumRule();
         boolean flag = value instanceof Integer;
         if (!flag) {
-            throw new ParameterException("注解key值必须为Integer类型");
+            try {
+                result = Integer.parseInt(value.toString());
+            } catch (Exception e) {
+                throw new ParameterException("注解key值必须为Integer类型");
+            }
+        } else {
+            if (null == enumClass) {
+                throw new AnnotationException("注解需要实现ISEnum接口");
+            }
+            if (!enumClass.isEnum()) {
+                throw new ParameterException(enumClass.getName() + "非枚举类型");
+            }
+            result = (Integer) value;
         }
-        Class<? extends ISEnum> enumClass = exportFieldBean.getEnumRule();
-        if (null == enumClass) {
-            throw new AnnotationException("注解需要实现ISEnum接口");
-        }
-        if (!enumClass.isEnum()) {
-            throw new ParameterException(enumClass.getName() + "非枚举类型");
-        }
-        Integer result = (Integer) value;
+
         ISEnum isEnum = resolveCustomEnum(result, enumClass);
         if (null == isEnum) {
             return "";
